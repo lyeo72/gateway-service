0505a2b867fde611a7a15f37826d7d0796b27ada
yeojin <lyeo7272@gmail.com>
2023-03-20 19:54:03 +0900

    apigateway-service

---
+++ src/main/java/com/example/gatewayservice/config/CustomFilter.java
@@ -0,0 +1,13 @@
+package com.example.gatewayservice.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.context.annotation.Configuration;
+
+//@Slf4j
+//@Configuration
+//public class CustomFilter extends AbstractGatewayFilterFactory {
+//
+//
+//}
